adfd088089a41174502a3814de59e1b3dccc2125
Devesh Chandra <deveshru2712@gmail.com>
2025-05-01 11:27:13 +0530

    fix a minor issue with protect route

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
 
-  if (!isUserAuthenticated) redirect("/sign-in");
+  if (isUserAuthenticated) redirect("/");
   return <div className="auth-layout">{children}</div>;
 };
 
