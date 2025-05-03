d00706f79baa97ea469b3e051b9ae0674737b5cf
Devesh Chandra <deveshru2712@gmail.com>
2025-05-03 12:49:25 +0530

    final push

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -1,7 +1,100 @@
+import { Button } from "@/components/ui/button";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getFeedBackByInterviewId,
+  getInterviewById,
+} from "@/lib/actions/general.action";
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = () => {
-  return <div></div>;
+const Page = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  const user = await getCurrentUser();
+
+  const interview = await getInterviewById(id);
+
+  if (!interview) redirect("/");
+
+  const feedback = await getFeedBackByInterviewId({
+    interviewId: id,
+    userId: user?.id!,
+  });
+
+  return (
+    <section className="section-feedback">
+      <div className="flex flex-row justify-center">
+        <h1 className="text-4xl font-semibold">
+          Feedback on the interview -{" "}
+          <span className="capitalize">{interview.role}</span>
+        </h1>
+      </div>
+      <div className="flex flex-row justify-center">
+        <div className="flex flex-row gap-5">
+          <div className="flex flex-row gap-2">
+            <Image src={"/star.svg"} alt={"star"} width={22} height={22} />
+            <p>
+              Overall Impression:{" "}
+              <span className="text-primary-200 font-bold">
+                {feedback?.totalScore}
+              </span>
+              /100
+            </p>
+          </div>
+          <div className="flex flex-row gap-2">
+            <Image
+              src={"/calendar.svg"}
+              alt="calendar"
+              width={22}
+              height={22}
+            />
+            <p>
+              {feedback?.createdAt
+                ? dayjs(feedback.createdAt).format("MMM D,YYYY h:mm A")
+                : "N/A"}
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr />
+
+      <p>{feedback?.finalAssessment}</p>
+      <div className="flex flex-col gap-4">
+        <h2>Breakdown of Interview:</h2>
+        {feedback?.categoryScores.map((category, index) => (
+          <div key={index}>
+            <p className="flex gap-1 font-bold">
+              {index + 1}.<span>{category.name}</span>({category.score}/100)
+            </p>
+            <p>{category.comment}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="buttons">
+        <Button className="btn-secondary flex-1">
+          <Link href={"/"} className="flex w-full justify-center">
+            <p className="text-sm font-semibold text-primary text-center">
+              Back to Dashboard
+            </p>
+          </Link>
+        </Button>
+
+        <Button className="btn-primary flex-1">
+          <Link
+            href={`/interview/${id}`}
+            className="flex w-full justify-center"
+          >
+            <p className="text-sm font-semibold text-primary text-center">
+              Retake the interview
+            </p>
+          </Link>
+        </Button>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -67,64 +67,32 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
   }
 }
 
-// export async function createFeedBack(params: CreateFeedbackParams) {
-//   const { interviewId, userId, transcript } = params;
-//   try {
-//     const formattedTranscript = transcript
-//       .map(
-//         (sentence: { role: string; content: string }) =>
-//           `- ${sentence.role} : ${sentence.content}\n`
-//       )
-//       .join();
+export async function getFeedBackByInterviewId(
+  params: GetFeedbackByInterviewIdParams
+) {
+  const { interviewId, userId } = params;
+  try {
+    const feedback = await db
+      .collection("feedbacks")
+      .where("interviewId", "==", interviewId)
+      .where("userId", "==", userId)
+      .limit(1)
+      .get();
 
-//     const {
-//       object: {
-//         totalScore,
-//         categoryScores,
-//         strengths,
-//         areasForImprovement,
-//         finalAssessment,
-//       },
-//     } = await generateObject({
-//       model: google("gemini-2.0-flash-001", {
-//         structuredOutputs: false,
-//       }),
-//       // how the text will look like
-//       schema: feedbackSchema,
-//       prompt: `You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
-//         Transcript:
-//         ${formattedTranscript}
-//         Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
-//         - **Communication Skills**: Clarity, articulation, structured responses.
-//         - **Technical Knowledge**: Understanding of key concepts for the role.
-//         - **Problem-Solving**: Ability to analyze problems and propose solutions.
-//         - **Cultural & Role Fit**: Alignment with company values and job role.
-//         - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
-//         `,
-//       system:
-//         "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
-//     });
-//     const feedBack = await db.collection("feedback").add({
-//       interviewId,
-//       userId,
-//       totalScore,
-//       categoryScores,
-//       strengths,
-//       areasForImprovement,
-//       finalAssessment,
-//       createdAt: new Date().toISOString(),
-//     });
-//     return {
-//       success: true,
-//       feedbackId: feedBack.id,
-//     };
-//   } catch (error) {
-//     console.log(error);
-//     return {
-//       success: false,
-//     };
-//   }
-// }
+    if (feedback.empty) {
+      return null;
+    }
+
+    const feedbackDoc = feedback.docs[0];
+
+    return {
+      id: feedbackDoc.id,
+      ...feedbackDoc.data(),
+    } as Feedback;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export async function createFeedBack(params: CreateFeedbackParams) {
   const { interviewId, userId, transcript, feedbackId } = params;
