23dbd2c0d7eea189eef3eb194df933bc7390ba6b
yashksaini-coder <ys3853428@gmail.com>
2025-05-03 13:27:28 +0530

    (fix): Hydration issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Mona_Sans } from "next/font/google";
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({
@@ -18,12 +20,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    const body = document.body;
+    body.removeAttribute("data-new-gr-c-s-check-loaded");
+    body.removeAttribute("cz-shortcut-listen");
+  }, []);
   return (
-    <html lang="en" className="dark">
-      <body className={`${monaSans.className} antialiased pattern`}>
+    <html lang="en" className="dark" suppressHydrationWarning={true}>
+      <body className={`${monaSans.className} antialiased pattern`} suppressHydrationWarning={true}>
         {children}
         <Toaster />
       </body>
     </html>
   );
 }
+
